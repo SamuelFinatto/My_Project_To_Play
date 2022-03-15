97fe47da4e7fb6c3658cbdd58a91e8b29e25adc4
Samuel Finatto <samueldfinatto@gmail.com>
2022-03-14 23:03:03 -0300

    included webPage
    Signed-off-by: Samuel Finatto <samueldfinatto@gmail.com>

--- WebFrontEnd/src/index.ts
+++ WebFrontEnd/src/index.ts
@@ -1,4 +1,18 @@
-import * as express from 'express';
-const app = express();
-app.use("/", (req: express.Request, res: express.Response) => res.send("Samuel"));
-app.listen(3000, () => console.log("listening"));
+// import * as express from 'express';
+// const app = express();
+// app.use("/", (req: express.Request, res: express.Response) => res.send("Samuel"));
+// app.listen(3000, () => console.log("listening"));
+
+import * as http from "http";
+import handler from "serve-handler";
+
+const server = http.createServer((request, response) => {
+  // You pass two more arguments for config and middleware
+  // More details here: https://github.com/zeit/serve-handler#options
+  return handler(request, response, { public: "public/" })
+})
+
+const port = process.env.PORT || 3000
+server.listen(port, () => {
+  console.log("Running at http://localhost:" + port)
+})
